b2ecc162ec653261bcc74f1869d0535079c6dd3d
Deniss Zerkalijs <deezee30@outlook.com>
2020-02-29 23:12:53 +0000

    Added src.ino for compatiblity with Arduino IDE in running the program

---
+++ src/src.ino
@@ -0,0 +1,4 @@
+// This file acts as an executable for the actual program. The main run file
+// is called "main.cpp" in this directory, and running this file via Arduino
+// will trigger the program to run too. This file was added to support the
+// execution of C++ files via Arduino software directly.
